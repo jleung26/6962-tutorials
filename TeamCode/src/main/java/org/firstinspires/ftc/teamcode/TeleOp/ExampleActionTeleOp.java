13d29b15fa08934b8cd0be7713f79f03835ca8a9
jleung26 <jleung26@punahou.edu>
2024-12-26 17:56:33 -1000

    new sample teleop
    This teleop uses bulk cache reading, and RR Actions for future macros. It also has foundation for future rising and falling edge detection logic.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/ExampleActionTeleOp.java
@@ -0,0 +1,63 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExampleActionTeleOp extends OpMode {
+    // Action stuff
+    private FtcDashboard dash = FtcDashboard.getInstance();
+    private List<Action> runningActions = new ArrayList<>();
+    private List<LynxModule> allHubs;
+    private MultipleTelemetry dashboardTelemetry = new MultipleTelemetry(telemetry, dash.getTelemetry());
+
+
+    // optimizing stuff apparently?
+    final Gamepad currentGamepad1 = new Gamepad();
+    final Gamepad currentGamepad2 = new Gamepad();
+    final Gamepad previousGamepad1 = new Gamepad();
+    final Gamepad previousGamepad2 = new Gamepad();
+
+    @Override
+    public void init() {
+        allHubs = hardwareMap.getAll(LynxModule.class);
+        for (LynxModule hub : allHubs) { hub.setBulkCachingMode(LynxModule.BulkCachingMode.MANUAL); }
+    }
+
+    @Override
+    public void loop() {
+        // clearing bulk cache
+        for (LynxModule hub : allHubs) {
+            hub.clearBulkCache();
+        }
+
+        // for rising edge detection
+        previousGamepad1.copy(currentGamepad1);
+        previousGamepad2.copy(currentGamepad2);
+
+        currentGamepad1.copy(gamepad1);
+        currentGamepad2.copy(gamepad2);
+
+        // for RR Action execution
+        TelemetryPacket packet = new TelemetryPacket();
+        List<Action> newActions = new ArrayList<>();
+        for (Action action : runningActions) {
+            action.preview(packet.fieldOverlay());
+            if (action.run(packet)) { // actually running actions
+                newActions.add(action); // if failed (run() returns true), try again
+            }
+        }
+        runningActions = newActions;
+        dash.sendTelemetryPacket(packet);
+
+        dashboardTelemetry.update();
+    }
+}
